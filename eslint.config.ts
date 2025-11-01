2c1c788dd8761edd260bc11900789147a2bb9f7f
Danil Golovin <destpb@gmail.com>
2025-11-01 12:09:05 +0300

    add eslint

---
+++ eslint.config.ts
@@ -0,0 +1,41 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        require: "readonly"
+      }
+    }
+  },
+  tseslint.configs.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  },
+  {
+    files: ["**/*.config.js", "**/*.config.ts", "rollup.config.js"],
+    rules: {
+      "@typescript-eslint/no-require-imports": "off"
+    }
+  },
+  {
+    files: ["**/*.{jsx,tsx}"],
+    rules: {
+      "react/react-in-jsx-scope": "off"
+    }
+  }
+]);
